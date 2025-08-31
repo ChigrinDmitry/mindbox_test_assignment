82078de4f628f26f8ae311020711275804c76cfe
Dmitry Chigrin <>
2025-08-31 16:34:41 +0500

    feat: implement task list component and task toggle functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import TaskInput from './components/TaskInput';
+import TaskList from './components/TaskList';
 import type { Task } from './types';
 import { useState } from 'react';
 import { v4 as uuid} from 'uuid';
@@ -12,10 +13,18 @@ function App() {
     setTasks(prev => [...prev, {id: uuid(), title, done: false }])
   };
 
+  const onToggle = (id: string) => {
+    setTasks(prev => 
+      prev.map(task => 
+        task.id === id ? { ...task, done: !task.done } : task)
+    )
+  }
+
   return (
     <>
       <h1>To-Do List</h1>
       <TaskInput onAdd={addTask}/>
+      <TaskList title='Все' tasks={tasks} onToggle={onToggle}></TaskList>
     </>
   )
 }
